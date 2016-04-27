e1f0b8d983f81b7a5eb944e4967a9c431157d23d
Bridget Dickens <badckns@yahoo.com>
2016-04-26 20:59:42 -0700

    added project page

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
 var port = process.env.PORT || 3000;
 
+//================
+// middleware
+//================
+app.set('views', 'views');
+
+app.engine('hbs', exphbs({
+   extname: 'hbs',
+   defaultLayout: 'main',
+   LayoutsDir: './views/layouts'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+//==============
+// routes
+//==============
 app.get('/', function(request, response) {
-   response.send('hi there');
+   //response.send('hi there');
+   response.render('home');
 });
+
+app.get('/projects', function(request, response) {
+  response.render('projects');
+});
+//==============
+// server
+//==============
 
 app.listen(port, function() {
   console.log('Server is running on ' + port);
