1a813284e360dccee0319211443d93ceb6223be6
Bridget Dickens <badckns@yahoo.com>
2016-04-26 19:53:08 -0700

    update

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.get('/', function(request, response) {
+   response.send('hi there');
+});
+
+app.listen(port, function() {
+  console.log('Server is running on ' + port);
+});
